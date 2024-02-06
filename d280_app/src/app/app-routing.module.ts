5a98334dd93296f96e02a643d1c367a9d94a5bda
Hamlet Valladares <sigorahv@gmail.com>
2024-02-06 15:46:44 -0500

    Completed steps A,C,D from the rubric

--- d280_app/src/app/app-routing.module.ts
+++ d280_app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component'; 
 
-const routes: Routes = [];
+//C.  Assign the map component to the default URL using an Angular routing module. 
+const routes: Routes = [
+  { path: '', component: WorldComponent }, // Default route
+  { path: 'map', component: WorldComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
